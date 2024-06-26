daba0900772ad9fda67b07f6ed77ab0e60e95ca3
Benjamin Leong <benri@users.noreply.github.com>
2024-06-26 14:57:26 -0700

    fix proxy

--- proxy.py
+++ proxy.py
@@ -1,14 +1,14 @@
+import json
 import os
-
-from typing import Optional
+from urllib.parse import urlparse, urljoin
+import io
 
 import typer
+from typing import Optional
 from typing_extensions import Annotated
 
 import asyncio
 from aiohttp import web, ClientSession
-
-from urllib.parse import urlparse, urljoin
 
 TARGET_SERVER = os.environ.get('LLM_URL')
 
@@ -29,6 +29,96 @@ models = {
         },
     ],
 }
+
+
+async def proxy_request(request, session, json_body=None):
+    path = request.url.path.rstrip('/')
+    target_url = request.app['target_url']
+    target_path = urljoin(target_url.path, path.lstrip('/'))
+
+    req_json = {}
+    if request.body_exists:
+        req_json = await request.json()
+
+    print("Request JSON:")
+    print(req_json)
+
+    # Send the original request to the target server with all headers
+    headers = {
+        'User-Agent': 'openai-proxy',
+        'Accept': '*/*',
+        'Authorization': 'Bearer ' + target_url.username,
+        'Content-Type': 'application/json',
+    }
+
+    u = target_url.scheme + '://' + target_url.hostname + target_path
+    return await session.request(request.method, str(u), headers=headers,
+                                 json=json_body if json_body else req_json)
+
+
+async def handle_chat_completions(request, session):
+    req_json = {}
+    if request.body_exists:
+        req_json = await request.json()
+
+    stream = req_json.get('stream', False)
+    if 'stream' in req_json:
+        req_json['stream'] = False
+
+    response = await proxy_request(request, session, json_body=req_json)
+
+    response_stream = io.BytesIO(await response.content.read())
+
+    res_json = json.loads(response_stream.read().decode('utf-8'))
+    print("Response:")
+    print(res_json)
+    response_stream.seek(0)
+
+    if stream:
+        # generate test events async
+        async def generate_events():
+            ret_json = res_json
+            ret_json['object'] = 'chat.completion.chunk'
+
+            msg = ret_json["choices"][0]["message"]
+            ret_json["choices"] = [{
+                "index": 0,
+                "delta": {
+                    "role": msg["role"],
+                    "content": msg["content"]
+                },
+                "finish_reason": None
+            }]
+
+            yield f"data: {json.dumps(ret_json)}\n\n"
+
+            json_stop = ret_json
+            json_stop["choices"] = [{
+                "index": 0,
+                "delta": {},
+                "finish_reason": "stop"
+            }]
+
+            yield f"data: {json.dumps(json_stop)}\n\n"
+
+            yield f"data: [DONE]"
+
+        resp = web.StreamResponse(status=response.status, headers={
+            "Content-Type": "text/event-stream",
+        })
+        await resp.prepare(request=request)
+        async for e in generate_events():
+            await resp.write(e.encode('utf-8'))
+    else:
+        resp = web.Response(body=response_stream, status=response.status, content_type='application/json')
+
+    return resp
+
+
+async def handle_default(request, session):
+    response = await proxy_request(request, session)
+
+    return web.Response(body=response.content, status=response.status, content_type=response.content_type)
 
 
 async def handler(request):
@@ -58,32 +148,10 @@ async def handler(request):
                 return web.json_response(status=200, data=models[0])
             elif path == '/models/sdxl':
                 return web.json_response(status=200, data=models[1])
+            elif path == '/chat/completions':
+                return await handle_chat_completions(request, session)
 
-            target_url = parsed_url
-            target_path = urljoin(target_url.path, path.lstrip('/'))
-
-            if request.body_exists and request.can_read_body:
-                json_body = await request.json()
-
-            # Send the original request to the target server with all headers
-            headers = {
-                'User-Agent': 'openai-proxy',
-                'Accept': '*/*',
-                'Authorization': 'Bearer ' + target_url.username,
-                'Content-Type': 'application/json',
-            }
-
-            print(json_body)
-            stream = json_body.get('stream', False)
-            json_body['stream'] = False
-
-            u = target_url.scheme + '://' + target_url.hostname + target_path
-
-            response = await session.request(method, str(u), headers=headers, json=json_body)
-
-            print(await response.text())
-
-            return web.Response(body=response.content, status=response.status, content_type=request.content_type)
+            return await handle_default(request, session)
 
 
 async def _relay_data(writer, reader):
@@ -104,6 +172,7 @@ def main(
     parsed_url = urlparse(server_url)
 
     app = web.Application()
+    app['target_url'] = parsed_url
     app.add_routes([web.route('*', '/{tail:.*}', handler)])
 
     web.run_app(app, host='0.0.0.0', port=port)
